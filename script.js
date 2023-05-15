c6ee23afa51baaab3e0ce2445f2e96480a62785f
Adriana20A <131642183+Adriana20A@users.noreply.github.com>
2023-05-15 08:58:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,440 @@
+let arr = [
+    {
+        name: "sweets 1",
+        link: "media/2.jpg",
+        description: "The recipe of this delicacy was created by us, and over time we are improving it more and more. With love for our customers.  Ingradients: wheat flour, yeast, water, eggs, milk, oil, salt, sugar, lemon juice, jam (blueberry and pistachio/raspberry and blackberry)",
+        description2: "Price: 50uah",
+        description3: "FRUIT PUFF PASTRY (blueberry and pistachio or raspberry and blackberry)",
+        category: 1 
+    },
+    {
+        name: "sweets 2",
+        link: "media/3.jpg",
+        description: "The refined and delicate taste of this product will surely lift everyone's mood. Ingradients: wheat flour, yeast, water, eggs, milk, salt, sugar, cheese, creamcheese, peanuts, raspberry, honey, vanillin, oil, butter.",  
+        description2: "Price: 35uah ",
+        description3: "СHEESECAKE WITH CHERRIES",
+        category: 1
+    },
+    {
+        name: "sweets 3",
+        link: "media/4.jpg",
+        description: "The interesting taste of ordinary patty.Everithing only for our costumers. Ingradients: wheat flour, yeast, water,milk, salt, sugar, vanillin, oil, cinamon.",
+        description2: "Price: 20uah",
+        description3: "BAKERY KNOTS (with cinamon) ",
+        category: 1
+    },
+    {
+        name: "sweets 4",
+        link: "media/5.jpg",
+        description: "CINAMON MUFFIN  Something between muffin and cinabon. Ingradients: wheat flour, backery powder, water, eggs, milk, salt, sugar, cinamon, oil, butter.",  
+        description2: "Price: 15uah ",
+        description3: "CINAMON MUFFIN",
+        category: 1
+    },
+    {
+        name: "sweets 5",
+        link: "media/6.jpg",
+        description: "It give you to feel a tasty of exotic. Ingradients: wheat flour, bakery powder, water, eggs, milk, salt, sugar, banana, oil, food coloring, vanillin, butter, chocolate, nuts. BANANA*S MUFFIN ",
+        description2: "Price: 20uah ",
+        description3: "BANANA'S MUFFIN ",
+        category: 1
+    },
+    {
+        name: "sweets 6",
+        link: "media/7.jpg",
+        description: "We added spinach for better tasty. Ingradients: wheat flour, yeast, water, eggs, milk, salt, sugar, cheese, creamcheese, sea salt, spinach, onion, oil, butter.",
+        description2: "Price: 40uah ",
+        description3: "SAVORY CHEESCAKE",
+        category: 2
+    },
+    {
+        name: "sweets 7",
+        link: "media/8.jpg",
+        description: "With tasty like in really italian restaurants. Better eatten hot. Ingradients: wheat flour, yeast, water, eggs, milk, salt, sugar, cheese, cherry,mozzarella, gouda, swisscheese, oil, butter.",
+        description2: "Price: 70uah",
+        description3: "PIZZA",
+        category: 2
+    },
+    {
+        name: "sweets 8",
+        link: "media/9.jpg",
+        description: "Traditional azerbaijani pie with herbs. Ingradients: wheat flour, water, salt, butter, onion, sorrel, cilantro, parsley, dill. ",
+        description2: "Price: 50uah",
+        description3: "QUATAB  ",
+        category: 2
+    },
+    {
+        name: "sweets 9",
+        link: "media/10.jpg",
+        description: " Tasty and fragrant pastries from Georgia. Ingradients: wheat flour, yeast, water, milk, oil, salt, sugar, cheese, egs.",
+        description2: "Price: 100uah",
+        description3: "KHACHAPURI",
+        category: 2
+    },
+    {
+        name: "sweets  ",
+        link: "media/11.jpg",
+        description: " Ingradients: wheat flour, water, milk, oil, salt, sugar, egs, bakery powder, lemon, pistachios, walnut.",
+        description2: "Price: 750uah/kg",
+        description3: "BAKLAVA(PISTACHIO+WALNUT)",
+        category: 4
+    },
+    {
+        name: "sweets 5",
+        link: "media/12.jpg",
+        description: "Ingradients: wheat flour, butter, yeast, water, milk, oil, salt, sugar, egs, lemon juice, salmon, avocado, salad, sesame, poppy seeds. ",
+        description2: "Price: 200uah  ",
+        description3: "CROISSANT(SALMON+AVOCADO) ",
+        category: 6
+    },
+    {
+        name: "sweets 5",
+        link: "media/13.jpg",
+        description: "Ingradients: wheat flour, water, milk, oil, butter, salt, sugar, egs, bakery powder, lemon, walnut.  ",
+        description2: "Price: 500uah/kg",
+        description3: "BAKLAVA(WALNUT) ",
+        category: 4
+    },
+    {
+        name: "sweets 5",
+        link: "media/14.jpg",
+        description: "Ingradients: wheat flour, yeast, water, milk, oil, salt, sugar, egs, butter, vanillin.",
+        description2: "Price: 20uah ",
+        description3: "CROISSANTS WIHT CUSTARD",
+        category: 1
+    },
+    {
+        name: "sweets 5",
+        link: "media/15.jpg",
+        description: "Ingradients: wheat flour, butter, yeast, water, milk, oil, salt, sugar, egs, lemon juice, salad, cucumber, boiled salmon. ",
+        description2: "Price: 150uah ",
+        description3: "CROISSANT(SALMON+AVOCADO)",
+        category: 6
+    },
+    {
+        name: "sweets 5",
+        link: "media/17.jpg",
+        description: "Ingradients: wheat flour, butter, yeast, water, milk, oil, salt, sugar, egs, lime juice, salad, tomato, cheese.",
+        description2: "Price: 150uah ",
+        description3: "CROISSANT(TOMATO+CHEESE) ",
+        category: 6
+    },
+    {
+        name: "sweets 5",
+        link: "media/18.jpg",
+        description: "Ingradients: wheat flour, butter, yeast, water, milk, oil, salt, sugar, egs, lemon juice, basilic, tomato, marinated bacon, french mustard. ",
+        description2: "Price: 170uah ",
+        description3: "CROISSANT(BOILED EGG+BACON) ",
+        category: 6
+    },
+    {
+        name: "sweets 5",
+        link: "media/19.jpg",
+        description: "  Ingradients: wheat flour, butter, yeast, water, milk, oil, salt, sugar, egs, sesame.",
+        description2: "Price: 25uah ",
+        description3: "COLLACH",
+        category: 3
+    },
+    {
+        name: "sweets 5",
+        link: "media/20.jpg",
+        description: " Ingradients: wheat flour, yeast, water, oil, salt, sugar, rye flour, leaven for bread.",
+        description2: "Price: 30uah ",
+        description3: "RYE WHEAT BREAD",
+        category: 3
+    },
+    {
+        name: "sweets 5",
+        link: "media/21.jpg",
+        description: " Ingradients: wheat flour, yeast, water, oil, salt, sugar.",
+        description2: "Price: 30uah ",
+        description3: "FRENCH BREAD ",
+        category: 3
+    },
+    {
+        name: "sweets 5",
+        link: "media/22.jpg",
+        description: "Ingradients: wheat flour, yeast, water, oil, salt, sugar. ",
+        description2: "Price: 50uah ",
+        description3: "BREAD   ",
+        category: 3
+    },
+    {
+        name: "sweets 5",
+        link: "media/23.jpg",
+        description: "  Ingradients: wheat flour, yeast, leaven for bread, water, oil, salt, sugar.  ",
+        description2: "Price: 60uah ",
+        description3: "BREAD(WITH LEAVEN) ",
+        category: 3
+    },
+    {
+        name: "sweets 5",
+        link: "media/24.jpg",
+        description: "   Ingradients: wheat flour, cornflour, leaven for bread, water, oil, salt, sugar.",
+        description2: "Price: 50uah ",
+        description3: "BREAD(WITH CORNFLOUR)",
+        category: 3
+    },
+    {
+        name: "sweets 5",
+        link: "media/25.jpg",
+        description: "  Ingradients: rice flour, leaven for bread, water, oil, salt, sugar.  ",
+        description2: "Price: 100uah",
+        description3: "BREAD(FROM RICEFLOUR)  ",
+        category: 3
+    },
+    {
+        name: "sweets 5",
+        link: "media/26.jpg",
+        description: "Ingradients: wheat flour, backery powder, water, yast, eggs, milk, salt, sugar, cinamon, oil, butter, vanillin. ",
+        description2: "Price: 60uah ",
+        description3: "CINABON  ",
+        category: 1
+    },
+    {
+        name: "sweets 5",
+        link: "media/27.jpg",
+        description: " Ingradients: wheat flour, backery powder, water, eggs, milk, salt, sugar, blueberry, oil, butter.",
+        description2: " Price: 17uah ",
+        description3: "MUFFIN WITH BLUEBERRY   ",
+        category: 1
+    },
+    {
+        name: "sweets 5",
+        link: "media/28.jpg",
+        description: " Ingradients: wheat flour, yast, water, eggs, milk, salt, sugar, pistachio, white chocolate, oil, butter.  ",
+        description2: "Price: 25uah",
+        description3: "DONUT(WITH PISTACHIO)  ",
+        category: 1
+    },
+    {
+        name: "sweets 5",
+        link: "media/29.jpg",
+        description: "  Ingradients: wheat flour, oatmeal, backery powder, eggs, salt, sugar, milk chocolate, oil, butter. ",
+        description2: "Price: 350uah/kg",
+        description3: "OATMEAL COOKIE (WITH MILK CHOCOLATE) ",
+        category: 5
+    },
+    {
+        name: "sweets 5",
+        link: "media/30.jpg",
+        description: "  Ingradients: wheat flour, pecane, yoghurt, backery powder, eggs, salt, sugar, dark chocolate, oil, butter.",
+        description2: " Price: 450uah/kg",
+        description3: "COOKIE (WITH DARK CHOCOLATE AND PECANE)",
+        category: 5
+    },
+    {
+        name: "sweets 5",
+        link: "media/31.jpg",
+        description: "Ingradients: wheat flour, white chocolate, yoghurt, backery powder, eggs, salt, sugar, milk chocolate, cookie Oreo, oil, butter.",
+        description2: " Price: 500uah/kg",
+        description3: "COOKIE (WITH WHITE, MILK CHOCOLATE AND OREO)  ",
+        category: 5
+    },
+    {
+        name: "sweets 5",
+        link: "media/32.jpg",
+        description: "  Ingradients: wheat flour, dark chocolate, yoghurt, backery powder, eggs, salt, sugar, cacao, , oil, butter. ",
+        description2: "Price: 400uah/kg",
+        description3: " CHOCOLATE COOKIE",
+        category: 5
+    },
+    {
+        name: "sweets 5",
+        link: "media/33.jpg",
+        description: " Ingradients: wheat flour, sea salt, backery powder, salt, sugar, dark chocolate, oil. ",
+        description2: "Price: 300uah/kg",
+        description3: "VEGAN COOKIE (WITH DARK CHOCOLATE AND SEA SALT) ",
+        category: 5
+    },
+    {
+        name: "sweets 5",
+        link: "media/34.jpg",
+        description: "Ingradients: wheat flour, almond, backery powder, salt, sugar, dark chocolate, oil. ",
+        description2: "Price: 300uah/kg",
+        description3: "VEGAN COOKIE (WITH ALMOND) ",
+        category: 5
+    },
+    {
+        name: "sweets 5",
+        link: "media/35.jpg",
+        description: " Ingradients: wheat flour, backery powder, sour cream, salt, sugar, milk chocolate, oil, butter. ",
+        description2: "Price: 350uah/kg",
+        description3: "COOKIE (WITH MILKCHOCOLATE)",
+        category: 5
+    },
+    {
+        name: "sweets 5",
+        link: "media/36.jpg",
+        description: "Ingradients: wheat flour, dark chocolate, yoghurt, backery powder, eggs, salt, sugar, pistachio, oil, butter. ",
+        description2: "Price: 700uah/kg",
+        description3: "COOKIE (WITH DARK CHOCOLATE AND PISTACHIO)",
+        category: 5
+    },
+    {
+        name: "sweets 5",
+        link: "media/37.jpg",
+        description: " Ingradients: wheat flour, milk chocolate, yoghurt, backery powder, eggs, salt, sugar, cacao, food color, oil, butter. ",
+        description2: "Price: 700uah/kg",
+        description3: "RED VELVET",
+        category: 5
+    },
+    {
+        name: "sweets 5",
+        link: "media/38.jpg",
+        description: " Ingradients: wheat flour, oatmeal flour, backery powder, eggs, salt, sugar, sour cream, oil, butter.",
+        description2: " Price: 320uah/kg",
+        description3: "OATMEAL COOKIE",
+        category: 5
+    },
+    {
+        name: "sweets 5",
+        link: "media/39.jpg",
+        description: " Ingradients: wheat flour, oatmeal, backery powder, eggs, salt, sugar, cacao, peanut, youghurt, milk chocolate, oil, butter. ",
+        description2: "Price: 350uah/kg",
+        description3: "OATMEAL COOKIE (WITH MILK CHOCOLATE, CACAO AND PEANUT) ",
+        category: 5
+    },
+    {
+        name: "sweets 5",
+        link: "media/40.jpg",
+        description: "  Ingradients: wheat flour, milk chocolte, eggs, milk, salt, sugar, water, condensed milk, butter, vanillin. ",
+        description2: "Price: 45uah",
+        description3: " ECLAIRS ",
+        category: 1
+    },
+    {
+        name: "sweets 5",
+        link: "media/41.jpg",
+        description: "Ingradients: wheat flour, water, milk, oil, salt, sugar, egs, bakery powder, lemon, pistachios.",
+        description2: "  Price: 800uah/kg",
+        description3: "BAKLAVA(PISTACHIO)  ",
+        category: 4
+    },
+    {
+        name: "sweets 5",
+        link: "media/42.jpg",
+        description: "Ingradients: wheat flour, backery powder, water, yast, cinabon, salt, sugar, cinamon, oil, powdered sugar, vanillin.",
+        description2: "Price: 20uah ",
+        description3: "VEGAN CINABON",
+        category: 1
+    }];
+
+    let barColors = [
+        "#8c483b",
+        "ivory",
+        "#4f2c2a",
+        "#fbc6a1",
+        "#c76046",
+        "#b1bebe",
+        "#663833",
+        "#f59b70",
+        "#4f3933",
+        "#f47a4f",
+        "#7e5547",
+        "#d68472",
+        "black",
+        "#58404c",
+        "#231210",
+        "#f7c47e",
+        "#3f211e",
+        "#eaaa72",
+        "#986c55",
+        "#6c4438",
+        "#b87e5c",
+        "776872",
+        "#945b46",
+        "#eeb2a3",
+        "#4e2727",
+        "#d4d4cc",
+        "#a69882",
+        "#fbc6a1",
+        "#4c4454",
+        "#d69568",
+        "#69402e",
+        "#c4ad99",
+        "#b46939",
+        "#f0a48e", ,
+        "#ecc0ad",
+        "#4e392c",
+        "#8f592c",
+        "#e5d2ca",
+        "#c0782b",
+    ];
+    
+
+let cookie = "user subscribed";
+
+function getElements(category) {
+    let cont = document.querySelector("#container");
+    let out = "<div class='row'>";
+    let i = 0;
+    arr.forEach(elem => {
+        if (category == null || elem.category == category) {
+            out += "<div class='col-sm-4'><img class='img-fluid' src=" + elem.link + " alt=" + elem.name + "<b class='name'>"  + elem.description3 + "</b><p class='description'>" + elem.description + "</p><b class='price'>"+ elem.description2 + "</b></div>"
+            i++;
+        }
+        if (i % 3 == 0 && 1) {
+            out += "</div><div class='row'>";
+        }
+    });
+    out += "</div>";
+    cont.innerHTML = out;
+}
+
+let modal = document.getElementById("myModal");
+let closeBtn = document.querySelector("#closeBtn");
+
+function close() {
+    modal.style.display = "none";
+}
+
+// setTimeout(() => {
+//     if (document.cookie != cookie) {
+//         modal.style.display = "block";
+
+//     }
+// }, 3000)
+
+let closeTime = 5;
+
+setInterval(() => {
+    if (modal.style.display == "block" && closeTime > 0) {
+        closeTime -= 1;
+        closeBtn.innerHTML = closeTime + " seconds";
+    } else if(closeTime <= 0){
+        closeBtn.innerHTML = "&times;";
+        closeBtn.addEventListener("click", close);
+    }
+}, 1000)
+
+
+function subscribe() {
+    document.cookie = cookie;
+    close();
+}
+
+const canvas = document.getElementById("myChart");
+const ctx = canvas.getContext("2d");
+
+ctx.font = "bold 48px serif";
+
+Chart.defaults.global.defaultFontColor = "#4f3933"; // туть
+
+new Chart("productsChart", {
+
+    type: "pie",
+    data: {
+        labels: arr.map(e => e.name),
+        datasets: [{
+            backgroundColor: barColors,
+            data: arr.map(e => e.category)
+        }]
+    },
+    options: {
+        title: {
+            display: true,
+            text: "Pasteries"
+        }
+    }
+});
